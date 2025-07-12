3d2467b627cedb9b8733ef40b992a2e150c65370
Sankalp Singh <singhsankalp880@gmail.com>
2025-07-13 01:24:42 +0530

    Correct portfolio structure

--- vite.config.server.ts
+++ vite.config.server.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       fileName: "production",
       formats: ["es"],
     },
-    outDir: "dist/server",
+    outDir: "dist/spa",
     target: "node22",
     ssr: true,
     rollupOptions: {
